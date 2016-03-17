3d7cb19145660385953b141ae8da97010171671b
Edward Paulet <espaulet@gmail.com>
2016-03-17 10:48:27 -0700

    add spec for uploaded test attributes

--- spec/uploader_spec.rb
+++ spec/uploader_spec.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 describe RainforestCli::Uploader do
-  let(:test_directory) { File.expand_path(File.join(__FILE__, '../validation-examples/correct_embeds')) }
   let(:options) { instance_double('RainforestCli::Options', token: 'foo', test_folder: test_directory) }
+  let(:progress_bar_double) { double('ProgressBar') }
   subject { described_class.new(options) }
 
   before do
+    allow(ProgressBar).to receive(:create).and_return(progress_bar_double)
+    allow(progress_bar_double).to receive(:increment)
     allow_any_instance_of(RainforestCli::Validator).to receive(:validate_with_exception!)
     allow_any_instance_of(RainforestCli::RemoteTests).to receive(:primary_key_dictionary)
       .and_return({})
@@ -12,12 +14,10 @@ describe RainforestCli::Uploader do
 
   describe '#upload' do
     context 'with new tests' do
-      let(:progress_bar_double) { double('ProgressBar') }
+      let(:test_directory) { File.expand_path(File.join(__FILE__, '../validation-examples/correct_embeds')) }
       let(:rf_test_double) { instance_double('Rainforest::Test', id: 123) }
 
       before do
-        allow(ProgressBar).to receive(:create).and_return(progress_bar_double)
-        allow(progress_bar_double).to receive(:increment)
         allow(subject).to receive(:upload_test)
       end
 
@@ -26,6 +26,29 @@ describe RainforestCli::Uploader do
           .and_return(rf_test_double).twice
         subject.upload
       end
+    end
+  end
+
+  describe 'uploaded test object' do
+    let(:test_directory) { File.expand_path(File.join(__FILE__, '../rainforest-example')) }
+    let(:test_double) { instance_double('Rainforest::Test', id: 123) }
+
+    before do
+      allow(Rainforest::Test).to receive(:create).and_return(test_double)
+    end
+
+    it 'contains the correct attributes' do
+      expect(Rainforest::Test).to receive(:update) do |_id, test_attrs|
+        expect(test_attrs).to include({
+                                        start_uri: '/start_uri',
+                                        title: 'Example Test',
+                                        source: 'rainforest-cli',
+                                        tags: ['foo', 'bar', 'baz'],
+                                        rfml_id: 'example_test'
+                                      })
+      end
+
+      subject.upload
     end
   end
 end

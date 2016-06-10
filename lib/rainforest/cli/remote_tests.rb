7943f68be3ee15b31cdc0234cb7947c59129af53
Edward Paulet <espaulet@gmail.com>
2016-06-09 23:41:38 -0700

    make an easy to way to fetch remote test ids without rainforest gem

--- lib/rainforest/cli/remote_tests.rb
+++ lib/rainforest/cli/remote_tests.rb
@@ -9,32 +9,45 @@ class RainforestCli::RemoteTests
     !Rainforest.api_key.nil?
   end
 
+  def tests
+    @tests ||= fetch_tests
+  end
+
   def rfml_ids
-    @rfml_ids ||= primary_key_dictionary.keys
+    @rfml_ids ||= tests.map { |t| t['rfml_id'] }
+  end
+
+  def primary_ids
+    @primary_ids ||= tests.map { |t| t['id'] }
   end
 
   def primary_key_dictionary
     @primary_key_dictionary ||= make_test_dictionary
   end
 
+  def make_test_dictionary
+    primary_key_dictionary = {}
+    tests.each do |rf_test|
+      primary_key_dictionary[rf_test['rfml_id']] = rf_test['id']
+    end
+    primary_key_dictionary
+  end
+
+  private
+
   def logger
     RainforestCli.logger
   end
 
-  def make_test_dictionary
-    primary_key_dictionary = {}
-
+  def fetch_tests
     if api_token_set?
-      rf_tests = @client.get('/tests/rfml_ids')
-      rf_tests.each do |rf_test|
-        primary_key_dictionary[rf_test['rfml_id']] = rf_test['id']
-      end
+      logger.info 'Syncing test data from server...'
+      test_list = @client.get('/tests/rfml_ids')
+      logger.info 'Fetch complete.'
+      test_list
     else
-      logger.info ''
       logger.info 'No API Token set. Using local tests only...'
-      logger.info ''
       []
     end
-    primary_key_dictionary
   end
 end

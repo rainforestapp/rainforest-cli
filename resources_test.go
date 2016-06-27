dc9f79b5f3d3230f4dacdcd218dced5a7029caf5
DyerSituation <ead2174@columbia.edu>
2016-06-27 16:24:06 -0700

    integration test now covers all resources

--- resources_test.go
+++ resources_test.go
@@ -39,3 +39,65 @@ func TestPrintSites(t *testing.T) {
 		t.Errorf("should have matched %v", pattern)
 	}
 }
+
+func TestPrintFolders(t *testing.T) {
+	siteResp := `[{"id": 707, "title": "Foo"}]`
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/folders.json" {
+			t.Errorf("fetchRource hit wrong endpoint (wanted /Folders.json but got %v)", r.URL.Path)
+		}
+		w.Write([]byte(siteResp))
+	}))
+	defer ts.Close()
+	baseURL = ts.URL
+
+	out = &bytes.Buffer{}
+	defer func() {
+		out = os.Stdout
+	}()
+
+	fetchResource("Folders")
+
+	pattern := `\| +707 +\| +Foo +\|`
+	matched, err := regexp.Match(pattern, out.(*bytes.Buffer).Bytes())
+	if err != nil {
+		t.Error("Error with pattern match:", err)
+	}
+
+	if !matched {
+		t.Logf("Table didn't match properly:")
+		t.Logf("%v\n", out)
+		t.Errorf("should have matched %v", pattern)
+	}
+}
+
+func TestBrowsersFolders(t *testing.T) {
+	siteResp := `{"available_browsers": [{"name": "firefox", "description": "Mozilla Firefox"}]}`
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/clients.json" {
+			t.Errorf("fetchRource hit wrong endpoint (wanted /clients.json but got %v)", r.URL.Path)
+		}
+		w.Write([]byte(siteResp))
+	}))
+	defer ts.Close()
+	baseURL = ts.URL
+
+	out = &bytes.Buffer{}
+	defer func() {
+		out = os.Stdout
+	}()
+
+	fetchResource("Browsers")
+
+	pattern := `\| +firefox +\| +Mozilla Firefox +\|`
+	matched, err := regexp.Match(pattern, out.(*bytes.Buffer).Bytes())
+	if err != nil {
+		t.Error("Error with pattern match:", err)
+	}
+
+	if !matched {
+		t.Logf("Table didn't match properly:")
+		t.Logf("%v\n", out)
+		t.Errorf("should have matched %v", pattern)
+	}
+}

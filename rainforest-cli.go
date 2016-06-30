6530ecfa09a89065ce288e767e1c587a7870a38b
DyerSituation <ead2174@columbia.edu>
2016-06-30 15:50:18 -0700

    parseCommands() can parse any slice that has commands

--- rainforest-cli.go
+++ rainforest-cli.go
@@ -11,19 +11,19 @@ var apiToken string
 var baseURL = "https://app.rainforestqa.com/api/1"
 var out io.Writer = os.Stdout
 
-func parseCommands() []string {
+func parseCommands(arguments []string) []string {
 	commands := make([]string, 0, 5)
-	for i := len(os.Args) - 1; i > 0; i-- {
-		if os.Args[i][0] != '-' {
-			commands = append(commands, os.Args[i])
-			os.Args = append(os.Args[:i], os.Args[i+1:]...)
+	for i := len(arguments) - 1; i > 0; i-- {
+		if arguments[i][0] != '-' {
+			commands = append(commands, arguments[i])
+			os.Args = append(arguments[:i], arguments[i+1:]...)
 		}
 	}
 	return commands
 }
 
 func main() {
-	commands := parseCommands()
+	commands := parseCommands(os.Args)
 	command := commands[0]
 
 	flag.StringVar(&apiToken, "token", "", "API token. You can find your account token at https://app.rainforestqa.com/settings/integrations")
@@ -38,6 +38,8 @@ func main() {
 	}
 
 	switch command {
+	case "runs":
+
 	case "sites":
 		printSites()
 	case "folders":

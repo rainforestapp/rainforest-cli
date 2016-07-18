1cc1cbcc234d5b06735a2188ad619fdb61e3e8ce
Edward Paulet <espaulet@gmail.com>
2016-07-18 11:22:11 -0700

    get absolute file path from relative file path in rfml

--- lib/rainforest/cli/file_uploader.rb
+++ lib/rainforest/cli/file_uploader.rb
@@ -2,6 +2,7 @@
 class RainforestCli::FileUploader
   def initialize(options)
     @test_files = RainforestCli::TestFiles.new(options)
+    @remote_tests = RainforestCli::RemoteTests.new(options.token)
   end
 
   # check files for the markup
@@ -14,27 +15,35 @@ class RainforestCli::FileUploader
     if tests_with_uploadables.empty?
       logger.info 'Nothing to upload'
     else
-      # logger.info 'Upload time!'
+      @remote_tests.primary_key_dictionary # fetch Test IDs
       logger.info "Starting file uploads for #{tests_with_uploadables.count} tests:"
-      tests_with_uploadables.each do |test|
-        logger.info "\t#{test.title}"
-        steps_with_uploadables = test.steps.select(&:has_uploadable?)
+      tests_with_uploadables.each do |rfml_test|
+        logger.info "\t#{rfml_test.title}"
+        steps_with_uploadables = rfml_test.steps.select(&:has_uploadable?)
         steps_with_uploadables.each do |step|
-          upload_from_step(step, test.rfml_id)
+          upload_from_step(step, rfml_test)
         end
       end
     end
   end
 
-  def upload_from_step(step, test_rfml_id)
-    upload_from_match_data(step.uploadable_in_action, test_rfml_id) if step.uploadable_in_action
-    upload_from_match_data(step.uploadable_in_response, test_rfml_id) if step.uploadable_in_response
+  def upload_from_step(step, rfml_test)
+    upload_from_match_data(step.uploadable_in_action, rfml_test) if step.uploadable_in_action
+    upload_from_match_data(step.uploadable_in_response, rfml_test) if step.uploadable_in_response
     # recursive for cases with multiple uploads
-    # upload_from_step(step, test_rfml_id) if step.has_uploadable?
+    # upload_from_step(step, rfml_test) if step.has_uploadable?
   end
 
-  def upload_from_match_data(match_data, test_rfml_id)
-    puts "At the upload step for #{test_rfml_id}"
+  def upload_from_match_data(match_data, rfml_test)
+    test_id = @remote_tests.primary_key_dictionary[rfml_test.rfml_id]
+    file_dir = File.dirname(rfml_test.file_name)
+    file_name = File.expand_path(File.join(file_dir, match_data[2]))
+
+    if File.exist?(file_name)
+      puts "/tests/#{test_id}/files"
+    else
+      logger.warn "\t\tNo such file exists: #{file_name}"
+    end
   end
 
   private

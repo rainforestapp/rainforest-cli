ab76eae33ef20ef9d3391b800f90f4fbf7b57ff3
Maciej Siwek <me@maciejsiwek.pl>
2016-11-11 18:43:15 +0000

    implement resources fetching

--- rainforest/resources.go
+++ rainforest/resources.go
@@ -1,14 +1,33 @@
 package rainforest
 
+import "strconv"
+
+// Resource interface is a standardized way of looking at the types returned from the resources
+// list API. It treats every resource as a pair of ID and description of entity under this ID.
+type Resource interface {
+	GetID() string
+	GetDescription() string
+}
+
 // Folder type represents a single folder returned by the API call for a list of folders
 type Folder struct {
 	ID    int    `json:"id"`
 	Title string `json:"title"`
 }
 
+// GetID returns the Folder ID
+func (f Folder) GetID() string {
+	return strconv.Itoa(f.ID)
+}
+
+// GetDescription returns the Folder name
+func (f Folder) GetDescription() string {
+	return f.Title
+}
+
 // GetFolders returns a slice of Folders (their names and IDs) which are available
 // for filtering RF tests.
-func (c *Client) GetFolders() (*[]Folder, error) {
+func (c *Client) GetFolders() ([]*Folder, error) {
 	// Prepare request
 	req, err := c.NewRequest("GET", "folders.json?page_size=100", nil)
 	if err != nil {
@@ -16,12 +35,12 @@ func (c *Client) GetFolders() (*[]Folder, error) {
 	}
 
 	// Send request and process response
-	var folderResp []Folder
+	var folderResp []*Folder
 	_, err = c.Do(req, &folderResp)
 	if err != nil {
 		return nil, err
 	}
-	return &folderResp, nil
+	return folderResp, nil
 }
 
 // Browser type represents a single browser returned by the API call for a list of browsers
@@ -30,9 +49,19 @@ type Browser struct {
 	Description string `json:"description"`
 }
 
+// GetID returns the Browser name
+func (b Browser) GetID() string {
+	return b.Name
+}
+
+// GetDescription returns the Browser description
+func (b Browser) GetDescription() string {
+	return b.Description
+}
+
 // GetBrowsers returns a slice of Browsers which are available for the client to run
 // RF tests against.
-func (c *Client) GetBrowsers() (*[]Browser, error) {
+func (c *Client) GetBrowsers() ([]*Browser, error) {
 	// Prepare request
 	req, err := c.NewRequest("GET", "clients.json", nil)
 	if err != nil {
@@ -41,7 +70,7 @@ func (c *Client) GetBrowsers() (*[]Browser, error) {
 
 	// browsersResp is a type returned by the API call for list of browsers
 	type browsersResponse struct {
-		AvailableBrowsers []Browser `json:"available_browsers"`
+		AvailableBrowsers []*Browser `json:"available_browsers"`
 	}
 	var browsersResp browsersResponse
 	// Send request and process response
@@ -49,7 +78,7 @@ func (c *Client) GetBrowsers() (*[]Browser, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &browsersResp.AvailableBrowsers, nil
+	return browsersResp.AvailableBrowsers, nil
 }
 
 // Site type represents a single site returned by the API call for a list of sites
@@ -58,8 +87,18 @@ type Site struct {
 	Name string `json:"name"`
 }
 
+// GetID returns the Site ID
+func (s Site) GetID() string {
+	return strconv.Itoa(s.ID)
+}
+
+// GetDescription returns the Site name
+func (s Site) GetDescription() string {
+	return s.Name
+}
+
 // GetSites fetches sites available to use during the RF runs.
-func (c *Client) GetSites() (*[]Site, error) {
+func (c *Client) GetSites() ([]*Site, error) {
 	// Prepare request
 	req, err := c.NewRequest("GET", "sites.json", nil)
 	if err != nil {
@@ -67,10 +106,10 @@ func (c *Client) GetSites() (*[]Site, error) {
 	}
 
 	// Send request and process response
-	var sitesResp []Site
+	var sitesResp []*Site
 	_, err = c.Do(req, &sitesResp)
 	if err != nil {
 		return nil, err
 	}
-	return &sitesResp, nil
+	return sitesResp, nil
 }
--- resources.go
+++ resources.go
@@ -1,36 +1,102 @@
 package main
 
-import "github.com/olekukonko/tablewriter"
+import (
+	"os"
 
-func printFolders() {
-	var table [][]string
-	var resBody foldersResp
-	getFolders("folders.json?page_size=100", &resBody)
-	table = resBody.displayTable()
-	printResource("Folder", table)
+	"github.com/olekukonko/tablewriter"
+	"github.com/rainforest/rainforest-cli/rainforest"
+	"github.com/urfave/cli"
+)
+
+// formatAsTable takes slice of a pointers of a type which represents one of the Resources
+// returned from API and returns it formated as a slice of [id, description] rows converted to string.
+func formatAsTable(resources []rainforest.Resource) [][]string {
+	// Create slice which will hold the resulting table
+	table := make([][]string, 0, len(resources))
+
+	// Iterate over slice of resources and convert them to tabular format
+	for _, resource := range resources {
+		tableRow := []string{resource.GetID(), resource.GetDescription()}
+		table = append(table, tableRow)
+	}
+	return table
 }
 
-func printSites() {
-	var table [][]string
-	var resBody sitesResp
-	getSites("sites.json", &resBody)
-	table = resBody.displayTable()
-	printResource("Site", table)
-}
+// printResourceTable uses olekukonko/tablewriter as a pretty printer
+// for the tabular resources we get from the API and formatted using formatAsTable.
+func printResourceTable(resourceName string, data [][]string) {
+	// Init tablewriter with STDOUT - can be potentially parametrized in the future
+	table := tablewriter.NewWriter(os.Stdout)
 
-func printBrowsers() {
-	var table [][]string
-	var resBody browsersResp
-	getBrowsers("clients.json", &resBody)
-	table = resBody.displayTable()
-	printResource("Browser", table)
-}
-
-func printResource(resource string, data [][]string) {
-	table := tablewriter.NewWriter(out)
-	table.SetHeader([]string{resource + " ID", resource + " Description"})
-	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	// Prepare the tablewriter
+	table.SetHeader([]string{resourceName + " ID", resourceName + " Description"})
+	table.SetBorders(tablewriter.Border{Left: true, Top: true, Right: true, Bottom: false})
 	table.SetCenterSeparator("|")
 	table.AppendBulk(data) // Add Bulk Data
+
+	// Render prints out the table to output specified during init.
 	table.Render()
+}
+
+// printFolders fetches and prints out the available folders from the API
+func printFolders(c *cli.Context) error {
+	// Fetch the list of folders from the Rainforest
+	folders, err := api.GetFolders()
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	// We need to manually cast to Resource type as we have a slice of the folders not resources
+	// and go will refuse to do it behind the scenes as it's O(n)
+	resources := make([]rainforest.Resource, len(folders))
+	for i, v := range folders {
+		resources[i] = rainforest.Resource(v)
+	}
+
+	// Now we can use it as an argument for our cool printing methods
+	tabular := formatAsTable(resources)
+	printResourceTable("Folder", tabular)
+	return nil
+}
+
+// printBrowsers fetches and prints out the browsers available to the client
+func printBrowsers(c *cli.Context) error {
+	// Fetch the list of browsers from the Rainforest
+	browsers, err := api.GetBrowsers()
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	// We need to manually cast to Resource type as we have a slice of the browsers not resources
+	// and go will refuse to do it behind the scenes as it's O(n)
+	resources := make([]rainforest.Resource, len(browsers))
+	for i, v := range browsers {
+		resources[i] = rainforest.Resource(v)
+	}
+
+	// Now we can use it as an argument for our cool printing methods
+	tabular := formatAsTable(resources)
+	printResourceTable("Browser", tabular)
+	return nil
+}
+
+// printSites fetches and prints out the defined sites
+func printSites(c *cli.Context) error {
+	// Fetch the list of sites from the Rainforest
+	sites, err := api.GetSites()
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	// We need to manually cast to Resource type as we have a slice of the sites not resources
+	// and go will refuse to do it behind the scenes as it's O(n)
+	resources := make([]rainforest.Resource, len(sites))
+	for i, v := range sites {
+		resources[i] = rainforest.Resource(v)
+	}
+
+	// Now we can use it as an argument for our cool printing methods
+	tabular := formatAsTable(resources)
+	printResourceTable("Site", tabular)
+	return nil
 }

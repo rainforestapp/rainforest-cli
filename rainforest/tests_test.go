b9e0434af3e920654d3095140dfc7e286fa3d194
Edward Paulet <espaulet@gmail.com>
2016-12-05 14:02:59 -0800

    add GET /tests/rfml_ids function to rainforest client

---
+++ rainforest/tests_test.go
@@ -0,0 +1,35 @@
+package rainforest
+
+import (
+	"encoding/json"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetRFMLIDs(t *testing.T) {
+	setup()
+	defer cleanup()
+
+	const reqMethod = "GET"
+
+	rfmlIDs := []Test{
+		{ID: 123, RFMLID: "abc"},
+		{ID: 456, RFMLID: "xyz"},
+	}
+
+	mux.HandleFunc("/tests/rfml_ids", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != reqMethod {
+			t.Errorf("Request method = %v, want %v", r.Method, reqMethod)
+		}
+
+		enc := json.NewEncoder(w)
+		enc.Encode(rfmlIDs)
+	})
+
+	out, _ := client.GetRFMLIDs()
+
+	if !reflect.DeepEqual(rfmlIDs, out) {
+		t.Errorf("Response expected = %v, actual %v", rfmlIDs, out)
+	}
+}

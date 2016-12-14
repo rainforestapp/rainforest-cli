3383b0ee98a5a4fc20b13c96ee14133fdba6b527
Edward Paulet <espaulet@gmail.com>
2016-12-14 01:14:19 -0800

    add test coverage for RFML writer redirects

--- rainforest/rfml_test.go
+++ rainforest/rfml_test.go
@@ -2,6 +2,7 @@ package rainforest
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
@@ -12,6 +13,7 @@ func TestWriteRFMLTest(t *testing.T) {
 	var buffer bytes.Buffer
 	writer := NewRFMLWriter(&buffer)
 
+	// Just test the required metadata first
 	rfmlID := "fake_rfml_id"
 	title := "fake_title"
 	startURI := "/path/to/nowhere"
@@ -55,6 +57,7 @@ func TestWriteRFMLTest(t *testing.T) {
 		}
 	}
 
+	// Now test all the headers
 	buffer.Reset()
 
 	siteID := 1989
@@ -79,5 +82,79 @@ func TestWriteRFMLTest(t *testing.T) {
 		if !strings.Contains(output, mustHave) {
 			t.Errorf("Missing expected string in writer output: %v", mustHave)
 		}
+	}
+
+	// Now test flattened steps
+	buffer.Reset()
+
+	firstStep := RFTestStep{
+		Action:   "first action",
+		Response: "first question",
+		Redirect: true,
+	}
+
+	secondStep := RFTestStep{
+		Action:   "second action",
+		Response: "second question",
+	}
+
+	test.Steps = []interface{}{firstStep, secondStep}
+
+	output = getOutput()
+
+	expectedStepText := fmt.Sprintf("%v\n%v\n\n%v\n%v", firstStep.Action, firstStep.Response,
+		secondStep.Action, secondStep.Response)
+	if !strings.Contains(output, expectedStepText) {
+		t.Error("Expected step text not found in writer output.")
+		t.Logf("Output:\n%v", output)
+		t.Logf("Expected:\n%v", expectedStepText)
+	}
+
+	// Test redirects for an embedded second step
+	buffer.Reset()
+
+	embeddedRFMLID := "embedded_test_rfml_id"
+	embeddedStep := RFEmbeddedTest{RFMLID: embeddedRFMLID}
+
+	test.Steps = []interface{}{firstStep, embeddedStep}
+
+	output = getOutput()
+
+	expectedStepText = fmt.Sprintf("%v\n%v\n\n# redirect: %v\n- %v", firstStep.Action, firstStep.Response,
+		embeddedStep.Redirect, embeddedStep.RFMLID)
+	if !strings.Contains(output, expectedStepText) {
+		t.Error("Expected step text not found in writer output.")
+		t.Logf("Output:\n%v", output)
+		t.Logf("Expected:\n%v", expectedStepText)
+	}
+
+	// Test redirects for an embedded first step
+	buffer.Reset()
+
+	test.Steps = []interface{}{embeddedStep, firstStep}
+
+	output = getOutput()
+
+	expectedStepText = fmt.Sprintf("- %v\n\n# redirect: %v\n%v\n%v", embeddedStep.RFMLID,
+		firstStep.Redirect, firstStep.Action, firstStep.Response)
+	if !strings.Contains(output, expectedStepText) {
+		t.Error("Expected step text not found in writer output.")
+		t.Logf("Output:\n%v", output)
+		t.Logf("Expected:\n%v", expectedStepText)
+	}
+
+	// Test redirects for a flat step after an embedded step that is not the first step
+	buffer.Reset()
+
+	test.Steps = []interface{}{firstStep, embeddedStep, secondStep}
+
+	output = getOutput()
+
+	expectedStepText = fmt.Sprintf("%v\n%v\n\n# redirect: %v\n- %v\n\n%v\n%v", firstStep.Action, firstStep.Response,
+		embeddedStep.Redirect, embeddedStep.RFMLID, secondStep.Action, secondStep.Response)
+	if !strings.Contains(output, expectedStepText) {
+		t.Error("Expected step text not found in writer output.")
+		t.Logf("Output:\n%v", output)
+		t.Logf("Expected:\n%v", expectedStepText)
 	}
 }

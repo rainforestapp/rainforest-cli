323bcadc0346d2eba68347aafa7587e46c5ee57c
Maciej Siwek <me@maciejsiwek.pl>
2017-01-25 17:13:40 +0000

    take only non-spaced part of rfmlID line as rfmlID

--- rainforest/rfml.go
+++ rainforest/rfml.go
@@ -60,8 +60,9 @@ func (r *RFMLReader) ReadAll() (*RFTest, error) {
 			if parsedRFTest.RFMLID != "" {
 				return parsedRFTest, &parseError{lineNum, "Only one RFML ID may be specified"}
 			}
-			// Handle shebang
-			parsedRFTest.RFMLID = strings.TrimSpace(line[2:])
+			// Trim shebang and then take only first part of id before any spaces
+			rfmlIDLine := strings.TrimSpace(line[2:])
+			parsedRFTest.RFMLID = strings.Split(rfmlIDLine, " ")[0]
 		} else if strings.HasPrefix(line, "#") {
 			// Handle hashed lines
 			content := line[1:]

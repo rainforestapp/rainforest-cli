fac33eb40f7096ce98ab56dcb2fd8377fbcf1f33
Maciej Siwek <me@maciejsiwek.pl>
2017-01-31 16:04:25 +0000

    bump up version

--- rainforest-cli.go
+++ rainforest-cli.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	// Version of the app in SemVer
-	version = "2.0.0-alpha.3"
+	version = "2.0.0-alpha.4"
 	// This is the default spec folder for RFML tests
 	defaultSpecFolder = "./spec/rainforest"
 )

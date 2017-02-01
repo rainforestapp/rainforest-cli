01cc31f15d115cf13b0d9bfefbe1b0f4fb9a6bdb
Edward Paulet <espaulet@gmail.com>
2017-01-31 21:53:54 -0800

    add site types to site command

--- rainforest/resources.go
+++ rainforest/resources.go
@@ -6,27 +6,10 @@ import (
 	"strconv"
 )
 
-// Resource interface is a standardized way of looking at the types returned from the resources
-// list API. It treats every resource as a pair of ID and description of entity under this ID.
-type Resource interface {
-	GetID() string
-	GetDescription() string
-}
-
 // Folder type represents a single folder returned by the API call for a list of folders
 type Folder struct {
 	ID    int    `json:"id"`
 	Title string `json:"title"`
-}
-
-// GetID returns the Folder ID
-func (f Folder) GetID() string {
-	return strconv.Itoa(f.ID)
-}
-
-// GetDescription returns the Folder name
-func (f Folder) GetDescription() string {
-	return f.Title
 }
 
 // GetFolders returns a slice of Folders (their names and IDs) which are available
@@ -81,16 +64,6 @@ type Browser struct {
 	Description string `json:"description"`
 }
 
-// GetID returns the Browser name
-func (b Browser) GetID() string {
-	return b.Name
-}
-
-// GetDescription returns the Browser description
-func (b Browser) GetDescription() string {
-	return b.Description
-}
-
 // GetBrowsers returns a slice of Browsers which are available for the client to run
 // RF tests against.
 func (c *Client) GetBrowsers() ([]Browser, error) {
@@ -115,18 +88,9 @@ func (c *Client) GetBrowsers() ([]Browser, error) {
 
 // Site type represents a single site returned by the API call for a list of sites
 type Site struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-// GetID returns the Site ID
-func (s Site) GetID() string {
-	return strconv.Itoa(s.ID)
-}
-
-// GetDescription returns the Site name
-func (s Site) GetDescription() string {
-	return s.Name
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Category string `json:"category"`
 }
 
 // GetSites fetches sites available to use during the RF runs.
--- resources.go
+++ resources.go
@@ -1,36 +1,24 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/olekukonko/tablewriter"
 	"github.com/rainforestapp/rainforest-cli/rainforest"
 	"github.com/urfave/cli"
 )
 
-// formatAsTable takes slice of a pointers of a type which represents one of the Resources
-// returned from API and returns it formated as a slice of [id, description] rows converted to string.
-func formatAsTable(resources []rainforest.Resource) [][]string {
-	// Create slice which will hold the resulting table
-	table := make([][]string, 0, len(resources))
-
-	// Iterate over slice of resources and convert them to tabular format
-	for _, resource := range resources {
-		tableRow := []string{resource.GetID(), resource.GetDescription()}
-		table = append(table, tableRow)
-	}
-	return table
-}
-
 // printResourceTable uses olekukonko/tablewriter as a pretty printer
 // for the tabular resources we get from the API and formatted using formatAsTable.
-func printResourceTable(resourceName string, data [][]string) {
+func printResourceTable(headers []string, rows [][]string) {
 	// Init tablewriter with out global var as a target
 	table := tablewriter.NewWriter(tablesOut)
 
 	// Prepare the tablewriter
-	table.SetHeader([]string{resourceName + " ID", resourceName + " Description"})
+	table.SetHeader(headers)
 	table.SetBorders(tablewriter.Border{Left: true, Top: true, Right: true, Bottom: false})
 	table.SetCenterSeparator("|")
-	table.AppendBulk(data) // Add Bulk Data
+	table.AppendBulk(rows) // Add Bulk Data
 
 	// Render prints out the table to output specified during init.
 	table.Render()
@@ -44,64 +32,63 @@ type resourceAPI interface {
 }
 
 // printFolders fetches and prints out the available folders from the API
-func printFolders(c cliContext, api resourceAPI) error {
+func printFolders(api resourceAPI) error {
 	// Fetch the list of folders from the Rainforest
 	folders, err := api.GetFolders()
 	if err != nil {
 		return cli.NewExitError(err.Error(), 1)
 	}
 
-	// We need to manually cast to Resource type as we have a slice of the folders not resources
-	// and go will refuse to do it behind the scenes as it's O(n)
-	resources := make([]rainforest.Resource, len(folders))
-	for i, v := range folders {
-		resources[i] = rainforest.Resource(v)
+	rows := make([][]string, len(folders))
+	for i, folder := range folders {
+		rows[i] = []string{strconv.Itoa(folder.ID), folder.Title}
 	}
 
-	// Now we can use it as an argument for our cool printing methods
-	tabular := formatAsTable(resources)
-	printResourceTable("Folder", tabular)
+	printResourceTable([]string{"Folder ID", "Folder Name"}, rows)
 	return nil
 }
 
 // printBrowsers fetches and prints out the browsers available to the client
-func printBrowsers(c cliContext, api resourceAPI) error {
+func printBrowsers(api resourceAPI) error {
 	// Fetch the list of browsers from the Rainforest
 	browsers, err := api.GetBrowsers()
 	if err != nil {
 		return cli.NewExitError(err.Error(), 1)
 	}
 
-	// We need to manually cast to Resource type as we have a slice of the browsers not resources
-	// and go will refuse to do it behind the scenes as it's O(n)
-	resources := make([]rainforest.Resource, len(browsers))
-	for i, v := range browsers {
-		resources[i] = rainforest.Resource(v)
+	rows := make([][]string, len(browsers))
+	for i, browser := range browsers {
+		rows[i] = []string{browser.Name, browser.Description}
 	}
 
-	// Now we can use it as an argument for our cool printing methods
-	tabular := formatAsTable(resources)
-	printResourceTable("Browser", tabular)
+	printResourceTable([]string{"Browser ID", "Browser Name"}, rows)
 	return nil
 }
 
 // printSites fetches and prints out the defined sites
-func printSites(c cliContext, api resourceAPI) error {
+func printSites(api resourceAPI) error {
 	// Fetch the list of sites from the Rainforest
 	sites, err := api.GetSites()
 	if err != nil {
 		return cli.NewExitError(err.Error(), 1)
 	}
 
-	// We need to manually cast to Resource type as we have a slice of the sites not resources
-	// and go will refuse to do it behind the scenes as it's O(n)
-	resources := make([]rainforest.Resource, len(sites))
-	for i, v := range sites {
-		resources[i] = rainforest.Resource(v)
+	humanizedSiteCategories := map[string]string{
+		"device_farm": "Device Farm",
+		"android":     "Android",
+		"ios":         "iOS",
+		"site":        "Site",
 	}
 
-	// Now we can use it as an argument for our cool printing methods
-	tabular := formatAsTable(resources)
-	printResourceTable("Site", tabular)
+	rows := make([][]string, len(sites))
+	for i, site := range sites {
+		category, ok := humanizedSiteCategories[site.Category]
+		if !ok {
+			category = site.Category
+		}
+		rows[i] = []string{strconv.Itoa(site.ID), site.Name, category}
+	}
+
+	printResourceTable([]string{"Site ID", "Site Name", "Category"}, rows)
 	return nil
 }

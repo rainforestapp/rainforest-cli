b40b8d2462daf6448930bb78ec93e0d6f94e9dc0
Edward Paulet <espaulet@gmail.com>
2017-10-13 11:36:48 -0700

    RF-3656 remove extra pointless function

--- reporter_test.go
+++ reporter_test.go
@@ -153,64 +153,90 @@ func newFakeReporterAPI(runID int, runTestDetails []rainforest.RunTestDetails) *
 	}
 }
 
-func TestCreateJunitTestReportSchema(t *testing.T) {
+func TestCreateJUnitReportSchema(t *testing.T) {
 	// Without failures
-	runID := 999
-	runTestTitle := "My title"
-	updatedAt := time.Now()
-	createdAt := updatedAt.Add(-10 * time.Minute)
+	now := time.Now()
+	runDesc := "very descriptive description"
+	totalTests := 1
+	totalNoResultTests := 0
+	totalFailedTests := 0
+	stateName := "complete"
 
-	tests := []rainforest.RunTestDetails{
-		{
-			Title:     runTestTitle,
-			CreatedAt: createdAt,
-			UpdatedAt: updatedAt,
-			Result:    "passed",
+	runDetails := rainforest.RunDetails{
+		ID:                 123,
+		Description:        runDesc,
+		TotalTests:         totalTests,
+		TotalNoResultTests: totalNoResultTests,
+		TotalFailedTests:   totalFailedTests,
+		StateDetails: rainforest.RunStateDetails{
+			Name:         stateName,
+			IsFinalState: true,
+		},
+		Timestamps: map[string]time.Time{
+			"created_at":  now.Add(-30 * time.Minute),
+			"in_progress": now.Add(-25 * time.Minute),
+			stateName:     now,
+		},
+		Tests: []rainforest.RunTestDetails{
+			{
+				Title:     "My test title",
+				CreatedAt: now.Add(-25 * time.Minute),
+				UpdatedAt: now,
+				Result:    "passed",
+			},
 		},
 	}
 
-	// api should not be used in this test case
-	api := newFakeReporterAPI(0, []rainforest.RunTestDetails{})
+	// api should not be used so input shouldn't matter
+	api := newFakeReporterAPI(-1, []rainforest.RunTestDetails{})
 
-	schema, err := createJunitTestReportSchema(runID, tests, api)
+	schema, err := createJUnitReportSchema(&runDetails, api)
 	if err != nil {
 		t.Errorf("Unexpected error returned by createJunitTestReportSchema: %v", err)
 	}
 
-	testSchema := schema[0]
-	expectedTestSchema := jUnitTestReportSchema{
-		Name: runTestTitle,
-		Time: 10 * time.Minute.Seconds(),
-	}
-
-	if !reflect.DeepEqual(testSchema, expectedTestSchema) {
-		t.Error("Incorrect JUnitTestReportSchema returned by createJunitTestReportSchema")
-		t.Errorf("Expected: %#v", expectedTestSchema)
-		t.Errorf("Actual: %#v", testSchema)
-	}
-
-	// With failures
-	runTestID := 123
-	failedBrowser := "chrome"
-	failedNote := "This note should appear"
-
-	tests = []rainforest.RunTestDetails{
-		{
-			ID:        runTestID,
-			Title:     runTestTitle,
-			CreatedAt: createdAt,
-			UpdatedAt: updatedAt,
-			Result:    "failed",
+	expectedSchema := jUnitReportSchema{
+		Name:     runDesc,
+		Errors:   totalNoResultTests,
+		Failures: totalFailedTests,
+		Tests:    totalTests,
+		Time:     30 * time.Minute.Seconds(),
+		TestCases: []jUnitTestReportSchema{
+			{
+				Name: runDetails.Tests[0].Title,
+				Time: 25 * time.Minute.Seconds(),
+			},
 		},
 	}
 
+	if !reflect.DeepEqual(expectedSchema, *schema) {
+		t.Error("Incorrect JUnitTestReportSchema returned by createJunitTestReportSchema")
+		t.Errorf("Expected: %#v", expectedSchema)
+		t.Errorf("Actual: %#v", schema)
+	}
+
+	// With failures
+	failedBrowser := "chrome"
+	failedNote := "This note should appear"
+
+	runDetails.TotalFailedTests = 1
+
+	failedTest := rainforest.RunTestDetails{
+		ID:        999888,
+		Title:     "My failed test",
+		CreatedAt: now.Add(-25 * time.Minute),
+		UpdatedAt: now,
+		Result:    "failed",
+	}
+	runDetails.Tests = []rainforest.RunTestDetails{failedTest}
+
 	apiTests := []rainforest.RunTestDetails{
 		{
-			ID:        runTestID,
-			Title:     runTestTitle,
-			CreatedAt: createdAt,
-			UpdatedAt: updatedAt,
-			Result:    "failed",
+			ID:        failedTest.ID,
+			Title:     failedTest.Title,
+			CreatedAt: failedTest.CreatedAt,
+			UpdatedAt: failedTest.UpdatedAt,
+			Result:    failedTest.Result,
 			Steps: []rainforest.RunStepDetails{
 				{
 					Browsers: []rainforest.RunBrowserDetails{
@@ -240,31 +266,34 @@ func TestCreateJunitTestReportSchema(t *testing.T) {
 		},
 	}
 
-	api = newFakeReporterAPI(runID, apiTests)
+	api = newFakeReporterAPI(runDetails.ID, apiTests)
 
 	var out bytes.Buffer
 	log.SetOutput(&out)
-	schema, err = createJunitTestReportSchema(runID, tests, api)
+	schema, err = createJUnitReportSchema(&runDetails, api)
 	log.SetOutput(os.Stdout)
+
 	if err != nil {
 		t.Errorf("Unexpected error returned by createJunitTestReportSchema: %v", err)
 	}
 
-	testSchema = schema[0]
-	expectedTestSchema = jUnitTestReportSchema{
-		Name: runTestTitle,
-		Time: 10 * time.Minute.Seconds(),
-		Failures: []jUnitTestReportFailure{
-			{
-				Type:    failedBrowser,
-				Message: failedNote,
+	expectedSchema.Failures = 1
+	expectedSchema.TestCases = []jUnitTestReportSchema{
+		{
+			Name: failedTest.Title,
+			Time: 25 * time.Minute.Seconds(),
+			Failures: []jUnitTestReportFailure{
+				{
+					Type:    failedBrowser,
+					Message: failedNote,
+				},
 			},
 		},
 	}
 
-	if !reflect.DeepEqual(testSchema, expectedTestSchema) {
+	if !reflect.DeepEqual(expectedSchema, *schema) {
 		t.Error("Incorrect JUnitTestReportSchema returned by createJunitTestReportSchema")
-		t.Errorf("Expected: %#v", expectedTestSchema)
-		t.Errorf("Actual: %#v", testSchema)
+		t.Errorf("Expected: %#v", expectedSchema)
+		t.Errorf("Actual: %#v", schema)
 	}
 }

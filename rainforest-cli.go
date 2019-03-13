8ebfe32d4ced82e9d5b69ce9dd6ef98e236d2678
shane <shane@rainforestapp.com>
2019-03-13 14:25:44 -0700

    RF-12966 - Update version and changelog

--- rainforest-cli.go
+++ rainforest-cli.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	// Version of the app in SemVer
-	version = "2.10.3"
+	version = "2.11.0"
 	// This is the default spec folder for RFML tests
 	defaultSpecFolder = "./spec/rainforest"
 )

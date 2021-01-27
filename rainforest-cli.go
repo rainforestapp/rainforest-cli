a8ee22fe655ed187303b6a95e65c4b71e4ee2187
Jonathan Barber <jonathan@rainforestapp.com>
2021-01-27 10:49:36 +0000

    RF-20670 Release --max-reruns fix
    Handle when the rainforest binary is not called `rainforest-cli`.

--- rainforest-cli.go
+++ rainforest-cli.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	// Version of the app in SemVer
-	version = "2.18.0"
+	version = "2.18.1"
 	// This is the default spec folder for RFML tests
 	defaultSpecFolder = "./spec/rainforest"
 )

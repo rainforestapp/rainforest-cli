f010994a557869d07861522fdaea4154b12f4d94
Russell Smith <ukd1@users.noreply.github.com>
2021-09-28 16:16:20 -0700

    RF-22483 Apparently there can silent failures with more than 4k characters - chocolatey/choco#2007

--- chocolatey/main.rb
+++ chocolatey/main.rb
@@ -109,8 +109,8 @@ xml = builder.package(xmlns: 'http://schemas.microsoft.com/packaging/2015/06/nus
     metadata.projectSourceUrl('https://github.com/rainforestapp/rainforest-cli')
     metadata.docsUrl('https://github.com/rainforestapp/rainforest-cli/blob/master/README.md')
     metadata.bugTrackerUrl('https://github.com/rainforestapp/rainforest-cli/issues')
-    metadata.description(File.read('../README.md').to_s)
-    metadata.releaseNotes(File.read('../CHANGELOG.md').to_s)
+    metadata.description(File.read('../README.md').to_s[0..4000])
+    metadata.releaseNotes(File.read('../CHANGELOG.md').to_s[0..4000])
   end
 
   package.files do |files|

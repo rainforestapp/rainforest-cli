48013cc6ecdbf4fbf29360eafcb4c988fd63a845
Russell Smith <ukd1@users.noreply.github.com>
2021-09-27 17:50:17 -0700

    Remove requires for tags, as apparently they don't run (#429)
    * Remove requires for tags, as apparently they don't run
    
    * Update the changelog 😇

--- rainforest-cli.go
+++ rainforest-cli.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	// Version of the app in SemVer
-	version = "2.22.1"
+	version = "2.22.2"
 	// This is the default spec folder for RFML tests
 	defaultSpecFolder = "./spec/rainforest"
 )

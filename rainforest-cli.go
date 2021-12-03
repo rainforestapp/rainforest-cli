fea8073b7e849b4b86cd48461369ffb16c046bfb
Paul Padier <paul.padier@rainforestapp.com>
2021-12-03 11:27:01 +0900

    RF-23055 Bump version to 2.25.0

--- rainforest-cli.go
+++ rainforest-cli.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	// Version of the app in SemVer
-	version = "2.24.0"
+	version = "2.25.0"
 	// This is the default spec folder for RFML tests
 	defaultSpecFolder = "./spec/rainforest"
 )
